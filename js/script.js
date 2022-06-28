1bb65b6950a433786dba0b0230de12899baa893a
Nike Fernandez <nikegfernandez@gmail.com>
2022-06-28 09:05:12 +0800

    Final

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ============================ Typing Animation ============================ 
+
+var typed = new Typed(".typing",{
+    strings:["Web Designer", "Web Developer", "Graphic Designer","Gamer"],
+    typeSpeed:100,
+    BackSpeed:1000,
+    loop:true
+});
